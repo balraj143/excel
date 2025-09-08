e59e153c11a2ed0f54a5c256ccc00d3a3ef77d7f
balraj143 <147017712+balraj143@users.noreply.github.com>
2025-09-07 20:47:48 -0700

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,4 +4,11 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", 
 });
 
+const axiosInstance = axios.create({
+  baseURL: "https://excel-ffjy.onrender.com", // your backend Render URL
+  withCredentials: true, // if you use cookies
+});
+
+export default axiosInstance;
+
 export default API;
