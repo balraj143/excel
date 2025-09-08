fe7873d68f4fa38e55de30f052de572fd4f4ffbd
balraj143 <147017712+balraj143@users.noreply.github.com>
2025-09-07 21:03:21 -0700

    Update login.jsx

--- frontend/src/auth/login.jsx
+++ frontend/src/auth/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import axiosInstance from "../src/utils/axiosInstance";
+import axiosInstance from "../utils/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import PasswordInput from "../components/PasswordInput";
 const Login = () => {
